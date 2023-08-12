ce461cfc0e6b5760a416681d4d2a446923f1210b
jahrim <jahrim.cesario2@studio.unibo.it>
2023-08-12 20:33:40 +0200

    chore: add MalformedInputException

--- src/main/scala/io/github/jahrim/chess/statistics/service/components/exceptions/StatisticsServiceException.scala
+++ src/main/scala/io/github/jahrim/chess/statistics/service/components/exceptions/StatisticsServiceException.scala
@@ -2,6 +2,7 @@ package io.github.jahrim.chess.statistics.service.components.exceptions
 
 /**
  * An [[Exception]] triggered by the statistics service.
+ *
  * @param message a detailed description of the [[Exception]].
  */
 class StatisticsServiceException(message: String) extends Exception(message)
